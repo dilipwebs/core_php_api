32a5235c800fd5bd5f9e56af5c744c29a2931d69
Dilip Webs <dilipwebs@gmail.com>
2023-04-09 20:40:51 +0530

    register api

--- register.php
+++ register.php
@@ -13,21 +13,28 @@ $data   =   json_decode(file_get_contents("php://input"),true);
 // $phone  = $_POST['phone'];
 // $city   = $_POST['city'];
 
-$name   = $data['name'];
-$email  = $data['email'];
-$password  = password_hash($data['password'], PASSWORD_BCRYPT);
-$phone  = $data['phone'];
-$city   = $data['city'];
 
-$query = "INSERT INTO users(name,email,password,phone,city) VALUES('".$name."','".$email."','".$password."','".$phone."','".$city."')";
 
-$result = mysqli_query($conn,$query);
+if($data){
 
-if($result){
-    http_response_code(200);
-    $data = ['msg'=>'Record inserted successfully'];
+    $name   = $data['name'];
+    $email  = $data['email'];
+    $password  = password_hash($data['password'], PASSWORD_BCRYPT);
+    $phone  = $data['phone'];
+    $city   = $data['city'];
+
+    $query = "INSERT INTO users(name,email,password,phone,city) VALUES('".$name."','".$email."','".$password."','".$phone."','".$city."')";
+
+    $result = mysqli_query($conn,$query);
+    if($result){
+        http_response_code(200);
+        $data = ['msg'=>'Record inserted successfully'];
+    }else{
+        http_response_code(401);
+        $data = ['msg'=>'Error: Record not inserted'];
+    }
 }else{
-    http_response_code(400);
+    http_response_code(401);
     $data = ['msg'=>'Error: Record not inserted'];
 }
 
